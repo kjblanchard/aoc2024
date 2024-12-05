3a1331c1b7253bce596e3dc9c494f6a768470eb6
Kevin Blanchard <blanchardkevinj@gmail.com>
2024-12-05 15:23:22 -0500

    part2 day 5

--- day5/main.go
+++ day5/main.go
@@ -11,9 +11,11 @@ import (
 )
 
 var (
-	rules       [][]int
-	pageUpdates [][]int
-	total       int
+	rules                    [][]int
+	pageUpdates              [][]int
+	incorrectUpdatesUnsorted [][]int
+	total                    int
+	part1                    = false
 )
 
 func checkError(e error) {
@@ -49,10 +51,11 @@ func checkUpdates() {
 			if first == -1 || second == -1 || second > first {
 				continue
 			}
+			incorrectUpdatesUnsorted = append(incorrectUpdatesUnsorted, update)
 			goodUpdate = false
 			break
 		}
-		if goodUpdate {
+		if part1 && goodUpdate {
 			updateLength := len(update)
 			if updateLength%2 == 0 {
 				log.Panicf("Somehow it is an even number?: %d", updateLength)
@@ -60,6 +63,36 @@ func checkUpdates() {
 			total += update[updateLength/2]
 		}
 	}
+}
+
+func sortIncorrectUpdate(update []int) {
+	var matchedRules [][]int
+	for _, rule := range rules {
+		first := slices.Index(update, rule[0])
+		second := slices.Index(update, rule[1])
+		// pt1,
+		// if first == -1 || second == -1 || second > first {
+		// pt2
+		if first == -1 || second == -1 {
+			continue
+		}
+		matchedRules = append(matchedRules, rule)
+	}
+	// topological sort, proper way to do this to save for later
+	g := &Graph{
+		edges:    make(map[int][]int),
+		vertices: update,
+	}
+	for _, rule := range matchedRules {
+		g.addEdge(rule[0], rule[1])
+	}
+	update = g.topologicalSort()
+	updateLength := len(update)
+	if updateLength%2 == 0 {
+		log.Panicf("Somehow it is an even number?: %d", updateLength)
+	}
+	num := update[updateLength/2]
+	total += num
 }
 
 func main() {
@@ -83,5 +116,43 @@ func main() {
 		}
 	}
 	checkUpdates()
+	if !part1 {
+		for _, incorrectUpdate := range incorrectUpdatesUnsorted {
+			sortIncorrectUpdate(incorrectUpdate)
+		}
+	}
 	log.Printf("Total number is: %d", total)
+}
+
+// Topological sort implementation for pt 2:
+// https://reintech.io/blog/topological-sorting-in-go
+type Graph struct {
+	edges    map[int][]int
+	vertices []int
+}
+
+func (g *Graph) addEdge(u, v int) {
+	g.edges[u] = append(g.edges[u], v)
+}
+
+func (g *Graph) topologicalSortUtil(v int, visited map[int]bool, stack *[]int) {
+	visited[v] = true
+	for _, u := range g.edges[v] {
+		if !visited[u] {
+			g.topologicalSortUtil(u, visited, stack)
+		}
+	}
+	*stack = append([]int{v}, *stack...)
+}
+
+func (g *Graph) topologicalSort() []int {
+	stack := []int{}
+	visited := make(map[int]bool)
+	for _, v := range g.vertices {
+		if !visited[v] {
+			g.topologicalSortUtil(v, visited, &stack)
+		}
+	}
+
+	return stack
 }
